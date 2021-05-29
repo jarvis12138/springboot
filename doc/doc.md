cd2fa29bb5b7d2d69eb4c16c6ce9f7e79b884c1a
Ehan <zengyiheng@dding.cn>
2021-05-29 21:31:51 +0800

    feat: 创建项目

---
+++ doc/doc.md
@@ -0,0 +1,6 @@
+##文档
+
+###请求参数和类型
+
+> 1、请求参数如： "p/{id}?page=1" 、 data: {name: "zhangsan", age: 19}
+> 2、请求类型如： "application/x-www-form-urlencoded" 、 "application/json" 等
---
+++ src/main/java/com/example/springboot/web/index.java
@@ -0,0 +1,16 @@
+package com.example.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Controller
+public class index {
+
+    @PostMapping(value = "p/{id}")
+    public @ResponseBody
+    Object test(@PathVariable int id, @RequestParam String page, @RequestBody Map<String, Object> map) {
+        return "id: " + id + ",page: " + page + ",name: " + map.get("name");
+    }
+}
