99fc7f8a1314f83b52ce44223b4ccdb97c687daa
Ehan <zengyiheng@dding.cn>
2021-06-12 20:43:07 +0800

    feat: jdbc链接

---
+++ src/main/java/com/example/springboot/controller/JDBCController.java
@@ -0,0 +1,61 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class JDBCController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    public User findUserById(@PathVariable("id") int id) {
+        String sql = "select * from User where id=?";
+        User user = jdbcTemplate.queryForObject(sql, new Object[]{id}, new BeanPropertyRowMapper<>(User.class));
+        return user;
+    }
+
+    @RequestMapping(value = "/user/list", method = RequestMethod.GET)
+    public List<User> userList() {
+        String sql = "select * from User";
+        List<User> maps = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
+        return maps;
+    }
+
+//    @RequestMapping(value = "/user/list", method = RequestMethod.GET)
+//    public List<Map<String, Object>> userList() {
+//        String sql = "select * from User";
+//        List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
+//        return maps;
+//    }
+
+    @RequestMapping(value = "/user/add", method = RequestMethod.GET)
+    public void addUser() {
+        String sql = "insert into User (id, name ,age) values (2,'zhangsan', '10')";
+        jdbcTemplate.update(sql);
+    }
+
+    @RequestMapping(value = "/user/update/{id}", method = RequestMethod.GET)
+    public void updateUser(@PathVariable("id") Integer id) {
+        String sql = "update User set name=?, age=?where id=" + id;
+        Object[] objects = new Object[2];
+        objects[0] = "lisi";
+        objects[1] = "19";
+        jdbcTemplate.update(sql, objects);
+    }
+
+    @RequestMapping(value = "/user/delete/{id}", method = RequestMethod.GET)
+    public void deleteUser(@PathVariable("id") Integer id) {
+        String sql = "delete from User where id=?";
+        jdbcTemplate.update(sql, id);
+    }
+}
--- src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.springboot;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class SpringbootApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        System.out.println(dataSource.getClass()); // 查看一下默认数据源
+
+        // 获得数据库连接
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
     }
 
 }
